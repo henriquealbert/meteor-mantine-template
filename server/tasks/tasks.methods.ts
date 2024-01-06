a8d477702ead70e3965913da6ed88abb4e73f3cc
Henrique A. Schmaiske <ishenriquealbert@gmail.com>
2024-01-06 18:41:54 -0300

    Add createTask, updateTask, and getTasks methods

--- server/tasks/tasks.methods.ts
+++ server/tasks/tasks.methods.ts
@@ -2,28 +2,43 @@ import { createModule } from "grubba-rpc";
 import { z } from "zod";
 import { TaskCollection } from "/server/tasks";
 
+/*
+ * Create task
+ */
 const createTaskSchema = z.object({
   title: z.string(),
   description: z.string().optional(),
   done: z.boolean().optional()
 });
-const createTask = async (params: z.infer<typeof createTaskSchema>) => {
+type CreateTaskParams = z.infer<typeof createTaskSchema>;
+const createTask = async (params: CreateTaskParams) => {
   const task = await TaskCollection.insertAsync(params);
   return task;
 };
 
+/*
+ * Update task
+ */
 const updateTaskSchema = z.object({
   id: z.string(),
   title: z.string().optional(),
   description: z.string().optional(),
   done: z.boolean().optional()
 });
-const updateTask = async (params: z.infer<typeof updateTaskSchema>) => {
+type UpdateTaskParams = z.infer<typeof updateTaskSchema>;
+const updateTask = async (params: UpdateTaskParams) => {
   const { id, ...update } = params;
   await TaskCollection.updateAsync(id, { $set: update });
 };
 
+/*
+ * Get all tasks
+ */
+const getTasksSchema = z.undefined();
+const getTasks = async () => TaskCollection.find({}).fetchAsync();
+
 export const tasksModule = createModule("tasks")
   .addMethod("createTask", createTaskSchema, createTask)
   .addMethod("updateTask", updateTaskSchema, updateTask)
+  .addMethod("getTasks", getTasksSchema, getTasks)
   .buildSubmodule();
