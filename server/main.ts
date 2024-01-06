3379a1adbc527e7b78d48b852b92e4279747b2cd
Henrique A. Schmaiske <ishenriquealbert@gmail.com>
2024-01-06 13:30:13 -0300

    Add tasks module to server main.ts

--- server/main.ts
+++ server/main.ts
@@ -1,6 +1,12 @@
 import { Meteor } from "meteor/meteor";
+import { createModule } from "grubba-rpc";
+import { tasksModule } from "/server/tasks";
 
 /**
  * This is the server-side entry point
  */
 Meteor.startup(async () => {});
+
+const api = createModule().addSubmodule(tasksModule).build();
+
+export type Api = typeof api;
